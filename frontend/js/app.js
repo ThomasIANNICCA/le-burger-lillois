642330c65a97398dc0d0fa70c8bb8cf091641065
ThomasIANNICCA <thomas.iannicca@oclock.school>
2024-04-09 18:32:28 +0200

    appel api fini + message d'erreur sélection + formatage heure

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -19,7 +19,7 @@ const app = {
             const allDistricts = await httpResponse.json()
 
             for(district of allDistricts){
-                app.addToDom(district)
+                app.initDistrict(district)
             }
         } catch (error) {
             return {
@@ -29,7 +29,7 @@ const app = {
         }
     }, 
 
-    addToDom: function(district){
+    initDistrict: function(district){
         const selectEl = document.querySelector('select')
 
         const optionEl = document.createElement('option')
@@ -49,22 +49,87 @@ const app = {
     },
 
     showRestaurant: async function(dataId){
-        try {
-            const httpResponse = await fetch("http://localhost:8000/api/district/" + dataId + '/restaurant')
+        
+        if (dataId == 0){
+            const resultContainer = document.querySelector('.right-section__container')
+            resultContainer.innerHTML=''
+            const alertElement = document.createElement('p')
+            alertElement.textContent = 'Veuillez sélectionner un quartier'
+            resultContainer.appendChild(alertElement)
+        } else{
+            try {
+                const httpResponse = await fetch("http://localhost:8000/api/district/" + dataId + '/restaurant')
 
-            const jsonRestaurant = await httpResponse.json()
-
-            console.log(jsonRestaurant);
-        } catch (error) {
-            return {
-                httpStatus: 500,
-                message: error
+                const jsonRestaurant = await httpResponse.json()
+        
+                const resultContainer = document.querySelector('.right-section__container')
+                resultContainer.innerHTML=''
+        
+                for(restaurant of jsonRestaurant){
+                    app.addNewElementsToDom(restaurant)
+                }
+            } catch (error) {
+                return {
+                    httpStatus: 500,
+                    message: error
+                }
             }
         }
-    }
-    
+    },
 
+    addNewElementsToDom: function(newElement){
+        const resultContainer = document.querySelector('.right-section__container')
+
+        // RESULTAT CONTAINER
+        const firstDivElement = document.createElement('div')
+        firstDivElement.classList.add('result-element')
+        resultContainer.appendChild(firstDivElement)
+
+        // RESULTAT IMAGE
+        const secondDivElement = document.createElement('div')
+        secondDivElement.classList.add('result-element__image')
+        firstDivElement.appendChild(secondDivElement)
+
+        const imgElement = document.createElement('img')
+        imgElement.src = newElement.picture
+        secondDivElement.appendChild(imgElement)
+
+        // RESULTAT DESCRIPTION
+        const thirdDivElement = document.createElement('div')
+        thirdDivElement.classList.add('result-element__description')
+        firstDivElement.appendChild(thirdDivElement)
+
+        const titleElement= document.createElement('h3')
+        titleElement.textContent = newElement.name
+        thirdDivElement.appendChild(titleElement)
+
+        const rankElement= document.createElement('p')
+        rankElement.textContent = 'Note : ' + newElement.rank + '/5'
+        thirdDivElement.appendChild(rankElement)
+
+        const ulElement=document.createElement('ul')
+        thirdDivElement.appendChild(ulElement)
+
+            //Configuration Li horaires et formatage d'heure
+        const HoursElement = document.createElement('li');
+
+            // Ouverture
+        const openHours = newElement.opening_hours
+        const newOpenHour = app.formatHours(openHours)
+
+            // Fermeture
+            const closeHours = newElement.closing_hours
+            const newCloseHour = app.formatHours(closeHours)
+        
+        HoursElement.textContent = newOpenHour + ' - ' + newCloseHour
+        ulElement.appendChild(HoursElement)
+    },
     
+    formatHours: function(hours){
+        const splitHours = hours.split(':')
+        const newHour = parseInt(splitHours[0])
+        return newHour + 'h'
+    }    
 }
 
 document.addEventListener('DOMContentLoaded', app.init())
